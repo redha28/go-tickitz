28d1039ff53f03c1b05a066ad6bbf48422151601
redha28 <redhadefinto28@gmail.com>
2025-04-28 02:15:07 +0700

    chore: initialize movies, showings, transactions, and admin repositories

--- internal/routes/v1/init.go
+++ internal/routes/v1/init.go
@@ -11,10 +11,14 @@ func InitRouter(router *gin.Engine, pg *pgxpool.Pool) {
 	v1 := router.Group("/api/v1")
 
 	usersRepo := repositories.NewUsersRepository(pg)
+	moviesRepo := repositories.NewMoviesRepository(pg)
+	showingsRepo := repositories.NewShowingsRepository(pg)
+	transactionsRepo := repositories.NewTransactionsRepository(pg)
+	adminRepo := repositories.NewAdminRepository(pg)
 
-	initAdminRouter(v1)
+	initAdminRouter(v1, adminRepo)
 	initAuthRouter(v1, usersRepo)
-	initMoviesRouter(v1)
-	initShowingsRouter(v1)
-	initTransactionsRouter(v1)
+	initMoviesRouter(v1, moviesRepo)
+	initShowingsRouter(v1, showingsRepo)
+	initTransactionsRouter(v1, transactionsRepo)
 }
