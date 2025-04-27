88118f5885cbe743996f0baf0f676f52a2fd8047
redha28 <redhadefinto28@gmail.com>
2025-04-28 02:17:46 +0700

    refactor: userres and updateprofilereq

--- internal/models/auth.model.go
+++ internal/models/auth.model.go
@@ -1,14 +1,15 @@
 package models
 
 type UserReq struct {
-	Email    string `json:"email" db:"email" validate:"required,email"`
-	Password string `json:"password" db:"password" validate:"required"`
-	Role     string `json:"role" db:"role"`
+	Email    string `json:"email" db:"email" binding:"required,email"`
+	Password string `json:"password" db:"password" binding:"required"`
+	Role     string `json:"role" db:"role" binding:"omitempty"`
 }
 
 type UserRes struct {
-	Email string `json:"email" db:"email"`
-	Role  string `json:"role" db:"role"`
+	Email  string `json:"email" db:"email"`
+	Role   string `json:"role" db:"role"`
+	AuthID string `json:"authId" db:"auth_id"`
 }
 
 type ProfileRes struct {
@@ -21,7 +22,10 @@ type ProfileRes struct {
 }
 
 type UpdateProfileReq struct {
-	Firstname string `json:"firstname" binding:"required"`
+	Firstname string `json:"firstname" binding:"omitempty"`
+	Lastname  string `json:"lastname" binding:"omitempty"`
+	Phone     string `json:"phone" binding:"omitempty"`
+	Picture   string `json:"picture" binding:"omitempty"`
 }
 
 type IdParams struct {
