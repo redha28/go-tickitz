cf3bde65ac13a70902ab6ac3c7a4cbac62329193
redha28 <redhadefinto28@gmail.com>
2025-05-06 10:31:28 +0700

    feat(router): add middleware for token verification and access control in admin movie routes

--- internal/routes/v1/admin.router.go
+++ internal/routes/v1/admin.router.go
@@ -2,25 +2,26 @@ package v1
 
 import (
 	handler "gotickitz/internal/handlers"
+	"gotickitz/internal/middlewares"
 	"gotickitz/internal/repositories"
 
 	"github.com/gin-gonic/gin"
 )
 
-func initAdminRouter(v1 *gin.RouterGroup, adminRepo *repositories.AdminRepository) {
+func initAdminRouter(v1 *gin.RouterGroup, adminRepo *repositories.AdminRepository, middleWare *middlewares.Middleware) {
 	// ADMIN
 	admin := v1.Group("/admin")
 	adminHandler := handler.NewAdminHandler(adminRepo)
 	// MOVIE
 	{
-		admin.GET("/movies", getMoviesAdminHandler)
-		admin.POST("/movies", adminHandler.CreateMovieHandler)
-		admin.PATCH("/movies/:id", adminHandler.UpdateMovieHandler)
-		admin.DELETE("/movies", adminHandler.DeleteMovieHandler)
+		// admin.GET("/movies", getMoviesAdminHandler)
+		admin.POST("/movies", middleWare.VerifyToken, middleWare.AccsessGate("admin"), adminHandler.CreateMovieHandler)
+		admin.PATCH("/movies/:id", middleWare.VerifyToken, middleWare.AccsessGate("admin"), adminHandler.UpdateMovieHandler)
+		admin.DELETE("/movies", middleWare.VerifyToken, middleWare.AccsessGate("admin"), adminHandler.DeleteMovieHandler)
 	}
 }
 
-func getMoviesAdminHandler(c *gin.Context) {}
+// func getMoviesAdminHandler(c *gin.Context) {}
 
 // func createMovieHandler(c *gin.Context)    {}
 // func updateMovieHandler(c *gin.Context) {}
