f6fba8df0951f3cec148cf9cfee60a0ec4cf5495
redha28 <redhadefinto28@gmail.com>
2025-05-06 10:31:45 +0700

    feat(router): update transactions router to include middleware for token verification and access control

--- internal/routes/v1/transactions.router.go
+++ internal/routes/v1/transactions.router.go
@@ -2,22 +2,23 @@ package v1
 
 import (
 	handler "gotickitz/internal/handlers"
+	"gotickitz/internal/middlewares"
 	"gotickitz/internal/repositories"
 
 	"github.com/gin-gonic/gin"
 )
 
-func initTransactionsRouter(v1 *gin.RouterGroup, transactionRepo *repositories.TransactionsRepository) {
+func initTransactionsRouter(v1 *gin.RouterGroup, transactionRepo *repositories.TransactionsRepository, middleWare *middlewares.Middleware) {
 
 	// TRANSACTIONS
 	transactions := v1.Group("/transactions")
 	transactionsHandler := handler.NewTransactionsHandler(transactionRepo)
 	{
-		transactions.GET("/:user_id", getTransactionsHandler)
-		transactions.POST("", transactionsHandler.CreateTransactionHandler)
+		transactions.GET("", middleWare.VerifyToken, middleWare.AccsessGate("user"), transactionsHandler.GetTransactionsHandler)
+		transactions.POST("", middleWare.VerifyToken, middleWare.AccsessGate("user"), transactionsHandler.CreateTransactionHandler)
 	}
 }
 
-func getTransactionsHandler(c *gin.Context) {}
+// func getTransactionsHandler(c *gin.Context) {}
 
 // func createTransactionHandler(c *gin.Context) {}
