e6512af55bc95cbc2bcffeb8e2f3708b513771b7
redha28 <redhadefinto28@gmail.com>
2025-05-06 10:29:43 +0700

    feat(auth): enhance validation for user registration and login

--- internal/handlers/auth.handler.go
+++ internal/handlers/auth.handler.go
@@ -3,8 +3,11 @@ package handler
 import (
 	"gotickitz/internal/models"
 	"gotickitz/internal/repositories"
+	"gotickitz/pkg"
 	"log"
 	"net/http"
+	"regexp"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -28,13 +31,35 @@ func (a *AuthHandler) RegisterHandler(ctx *gin.Context) {
 func registerHandler(c *gin.Context, a *AuthHandler) {
 	var userReq models.UserReq
 	if err := c.ShouldBindJSON(&userReq); err != nil {
+		if strings.Contains(err.Error(), "Field validation for 'Password'") {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"msg": "Password length must be at least 8 characters",
+			})
+			return
+		}
 		log.Println(err.Error())
 		c.JSON(http.StatusBadRequest, gin.H{
 			"msg": "Invalid input",
 		})
 		return
 	}
-	result, findEmail, err := a.UsersRepository.UseRegister(c.Request.Context(), userReq)
+	if !isValidEmail(userReq.Email) {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"msg": "Invalid email format",
+		})
+		return
+	}
+	hash := pkg.InitHashConfig()
+	hash.UseDefaultConfig()
+	hashedPass, err := hash.GenHashedPassword(userReq.Password)
+	if err != nil {
+		log.Println(err.Error())
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"msg": "Hash failed",
+		})
+		return
+	}
+	result, findEmail, err := a.UsersRepository.UseRegister(c.Request.Context(), userReq, hashedPass)
 
 	if err != nil {
 		log.Println(err.Error())
@@ -58,13 +83,26 @@ func registerHandler(c *gin.Context, a *AuthHandler) {
 func loginHandler(c *gin.Context, a *AuthHandler) {
 	var userReq models.UserReq
 	if err := c.ShouldBindJSON(&userReq); err != nil {
+		if strings.Contains(err.Error(), "Field validation for 'Password'") {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"msg": "Password length must be at least 8 characters",
+			})
+			return
+		}
 		log.Println(err.Error())
 		c.JSON(http.StatusBadRequest, gin.H{
 			"msg": "Invalid input",
 		})
 		return
 	}
-	result, err := a.UsersRepository.UseLogin(c.Request.Context(), userReq.Email, userReq.Password)
+	log.Println("[DEBUG] USERREQ:", userReq.Email)
+	if !isValidEmail(userReq.Email) {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"msg": "Invalid email format",
+		})
+		return
+	}
+	result, err := a.UsersRepository.UseLogin(c.Request.Context(), userReq.Email)
 	if err != nil {
 		log.Println(err.Error())
 		c.JSON(http.StatusInternalServerError, gin.H{
@@ -78,8 +116,39 @@ func loginHandler(c *gin.Context, a *AuthHandler) {
 		})
 		return
 	}
+	// log.Println("[DEBUG] PASSWORD:", result.Pass)
+	hash := pkg.InitHashConfig()
+	hash.UseDefaultConfig()
+	valid, err := hash.CompareHashAndPassword(result.Pass, userReq.Password)
+	if err != nil {
+		log.Println(err.Error())
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"msg": "Terjadi kesalahan pada server",
+		})
+		return
+	}
+	if !valid {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"msg": "Email atau password salah",
+		})
+		return
+	}
+	payload := pkg.NewPayload(result.AuthID, result.Role)
+	token, err := payload.GenerateToken()
+	if err != nil {
+		log.Println(err.Error())
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"msg": "Internal server error",
+		})
+	}
 	c.JSON(http.StatusOK, gin.H{
 		"msg":  "login success",
-		"data": result,
+		"data": map[string]string{"id": result.AuthID, "role": result.Role, "token": token},
 	})
+}
+
+func isValidEmail(email string) bool {
+	// Menggunakan regex untuk memastikan format email benar
+	re := regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`)
+	return re.MatchString(email)
 }
