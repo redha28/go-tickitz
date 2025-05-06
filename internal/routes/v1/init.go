e0b1c6244d846b22937f15609b2bbef9cbfd6af5
redha28 <redhadefinto28@gmail.com>
2025-05-06 10:31:34 +0700

    feat(router): enhance InitRouter to include Redis client and middleware for improved admin and user route handling

--- internal/routes/v1/init.go
+++ internal/routes/v1/init.go
@@ -1,24 +1,29 @@
 package v1
 
 import (
+	"gotickitz/internal/middlewares"
 	"gotickitz/internal/repositories"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/redis/go-redis/v9"
 )
 
-func InitRouter(router *gin.Engine, pg *pgxpool.Pool) {
+func InitRouter(router *gin.Engine, pg *pgxpool.Pool, rdb *redis.Client) {
 	v1 := router.Group("/api/v1")
 
 	usersRepo := repositories.NewUsersRepository(pg)
-	moviesRepo := repositories.NewMoviesRepository(pg)
+	moviesRepo := repositories.NewMoviesRepository(pg, rdb)
 	showingsRepo := repositories.NewShowingsRepository(pg)
 	transactionsRepo := repositories.NewTransactionsRepository(pg)
-	adminRepo := repositories.NewAdminRepository(pg)
+	adminRepo := repositories.NewAdminRepository(pg, rdb)
 
-	initAdminRouter(v1, adminRepo)
-	initAuthRouter(v1, usersRepo)
+	// Middleware
+	middlewares := middlewares.InitMiddleware()
+
+	initAdminRouter(v1, adminRepo, middlewares)
+	initUsersRouter(v1, usersRepo, middlewares)
 	initMoviesRouter(v1, moviesRepo)
 	initShowingsRouter(v1, showingsRepo)
-	initTransactionsRouter(v1, transactionsRepo)
+	initTransactionsRouter(v1, transactionsRepo, middlewares)
 }
